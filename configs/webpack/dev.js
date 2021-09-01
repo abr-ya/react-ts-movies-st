a9000c509e7ae11f9b7d1ce74c5da1cdbfc6a6c4
abr-ya <gamer05@list.ru>
2021-09-01 18:45:50 +0300

    Dotenv to common config

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -1,5 +1,5 @@
 // development config
-const Dotenv = require("dotenv-webpack");
+// const Dotenv = require("dotenv-webpack");
 const { merge } = require("webpack-merge");
 const commonConfig = require("./common");
 
@@ -15,5 +15,5 @@ module.exports = merge(commonConfig, {
     historyApiFallback: true, // fixes error 404-ish errors when using react router :see this SO question: https://stackoverflow.com/questions/43209666/react-router-v4-cannot-get-url
   },
   devtool: "cheap-module-source-map",
-  plugins: [new Dotenv()],
+  plugins: [],
 });
